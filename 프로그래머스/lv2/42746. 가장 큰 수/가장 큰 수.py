41224e0c7005c1b0c86247e32910fb7e12f94d39
cryoon <anthonychangrok@gmail.com>
2022-12-10 01:40:20 +0900

    [level 2] Title: 가장 큰 수, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,10 +1,9 @@
 def solution(numbers):
     str_numbers = [str(i) for i in numbers]
-    
-    # lambda x: x*3 이유는 자릿수가 4개 이하이니까 문자열을 3번 이상 반복해서 비교를 해야함
-    str_numbers.sort(key=lambda x: x*3, reverse=True)
-    
-    # [0, 0, 0, 0] test case -> "0000" (x) / "0" (o)
-    answer = str(int(''.join(str_numbers)))
-    return answer
 
+    # lambda x: x*3 이유는 자릿수가 4개 이하이니까 문자열을 3번 이상 반복해서 비교를 해야함
+    str_numbers.sort(key=lambda x: x * 3, reverse=True)
+
+    # [0, 0, 0, 0] test case -> "0000" (x) / "0" (o)
+    answer = str(int("".join(str_numbers)))
+    return answer
