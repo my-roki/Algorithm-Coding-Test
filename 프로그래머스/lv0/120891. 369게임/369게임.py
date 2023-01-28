f5c335851e50908b2966fd993b86237a8e082a70
cryoon <anthonychangrok@gmail.com>
2023-01-28 18:08:56 +0900

    [level 0] Title: 369게임, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.py
+++ 프로그래머스/lv0/120891. 369게임/369게임.py
@@ -1,3 +1,3 @@
 def solution(order):
     order_str = str(order)
-    return (order_str.count('3') + order_str.count('6')+ order_str.count('9'))
+    return order_str.count('3') + order_str.count('6')+ order_str.count('9')
