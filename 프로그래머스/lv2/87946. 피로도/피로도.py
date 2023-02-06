8f51ed4ace1434e55f3a6be42b6b107cdae01b3d
cryoon <anthonychangrok@gmail.com>
2023-02-07 00:08:35 +0900

    피로도.py 주석 추가

--- 프로그래머스/lv2/87946. 피로도/피로도.py
+++ 프로그래머스/lv2/87946. 피로도/피로도.py
@@ -8,18 +8,22 @@ from itertools import permutations
 
 
 def solution(k, dungeons):
-    answer = -1
+    # 각 던전을 돌 수 있는 모든 경우의 수
     candidate = permutations(dungeons, len(dungeons))
+    answer = -1
     for c_dugeons in candidate:
         cnt = 0  # 탐험 횟수 초기화
         new_k = k  # 스테미나 초기화
+
+        # 각 던전을 도는 시뮬레이션
         for dugeon in c_dugeons:
             if new_k >= dugeon[0]:
                 new_k -= dugeon[1]
                 cnt += 1
-            else:
+            else:  # 더이상 던전을 돌 최소 스테미너가 없으면 반복문 종료
                 break
 
+        # 최댓값 비교
         if answer <= cnt:
             answer = cnt
 
