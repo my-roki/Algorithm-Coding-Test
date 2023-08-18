45e683d11cb9cf9d1075f706f7f1491168437e27
cryoon <yoonchangrock@gmail.com>
2023-08-18 11:04:51 +0900

    [level 2] Title: 문자열 압축, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.py
@@ -1,25 +1,26 @@
-import math
-
-def compress(n,string):
-    result = ""
-    count = 1
-    for i in range(0, len(string), n):
-        if string[i : i + n] == string[i + n : i + (2 * n)]:
-            count += 1
-        else:
-            if count == 1:
-                result += string[ i : i + n] 
-            else:
-                result += f"{count}{string[i : i + n]}"
-                count = 1
-    return result
-            
-
-
 def solution(s):
-    answer = len(s)
-    for i in range(1, math.floor(len(s)/2)+1):
-        result = len(compress(i, s))
-        if result < answer:
-            answer = result
-    return answer
+    length = len(s)
+    min_length = length
+    
+    for i in range(1, length // 2 + 1):
+        count = 1
+        temp = s[:i]
+        result = ""
+        for j in range(i, length, i):
+            if temp == s[j:j + i]:
+                count += 1
+            else:
+                result += str(count) + temp if count > 1 else temp
+                temp = s[j:j + i]
+                count = 1
+        result += str(count) + temp if count > 1 else temp
+        min_length = min(min_length, len(result))
+    return min_length
+
+
+if __name__ == "__main__":
+    print(solution("aabbaccc"))  # 7
+    print(solution("ababcdcdababcdcd"))  # 9
+    print(solution("abcabcdede"))  # 8
+    print(solution("abcabcabcabcdededededede"))  # 14
+    print(solution("xababcdcdababcdcd"))  # 17
