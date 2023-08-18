44994807c8092600b1dc66f1baf71d8acd557447
cryoon <yoonchangrock@gmail.com>
2023-08-18 11:29:50 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.py
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -1,13 +1,22 @@
-def solution(n):
-    ternary_num_reverse = ""
-    while n != 0:
-        quo = n // 3
-        div = n % 3
-        ternary_num_reverse += str(div)
-        n = quo
+def solution(n: int) -> int:
+    return convert3to10(convert10to3(n)[::-1])
 
+
+# 10진법 -> 3진법 변환
+def convert10to3(n: int) -> str:
+    T = "0123456789"
+    q, r = divmod(n, 3)
+    return T[r] if q == 0 else convert10to3(q) + T[r]
+
+
+# 3진법 -> 10진법 변환
+def convert3to10(n: str) -> int:
     answer = 0
-    for idx, i in enumerate(ternary_num_reverse[::-1]):
-        answer += int(i) * (3**idx)
-
+    for idx, num in enumerate(n[::-1]):
+        answer += int(num) * (3**idx)
     return answer
+
+
+if __name__ == "__main__":
+    print(solution(45))  # 7
+    print(solution(125))  # 229
