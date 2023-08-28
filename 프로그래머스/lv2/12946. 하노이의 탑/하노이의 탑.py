4e3756f224fb84979358313c2131bc45dc66b807
cryoon <yoonchangrock@gmail.com>
2023-08-28 11:18:41 +0900

    [level 2] Title: 하노이의 탑, Time: 12.72 ms, Memory: 18.8 MB -BaekjoonHub

--- 프로그래머스/lv2/12946. 하노이의 탑/하노이의 탑.py
+++ 프로그래머스/lv2/12946. 하노이의 탑/하노이의 탑.py
@@ -1,13 +1,14 @@
-def hanoi(answer, depth, fromm, to, mid):
-    if depth ==1:
-        answer.append([fromm,to])
-    else:
-        hanoi(answer, depth-1, fromm, mid, to)
-        answer.append([fromm,to])
-        hanoi(answer, depth-1, mid, to, fromm)
-        
-
 def solution(n):
-    answer = []
-    hanoi(answer, n, 1 ,3 , 2)
-    return answer
+    return hanoi(n, 1, 3, 2)
+
+def hanoi(n, start, end, via):
+    if n == 1:
+        return [[start, end]]
+    else:
+        return hanoi(n-1, start, via, end) + [[start, end]] + hanoi(n-1, via, end, start)
+    
+
+if __name__ == '__main__':
+    print(solution(2))  # [ [1,2], [1,3], [2,3] ]
+    print(solution(3))  # [ [1,3], [1,2], [3,2], [1,3], [2,1], [2,3], [1,3] ]
+    print(solution(4))  # [[1, 2], [1, 3], [2, 3], [1, 2], [3, 1], [3, 2], [1, 2], [1, 3], [2, 3], [2, 1], [3, 1], [2, 3], [1, 2], [1, 3], [2, 3]]
