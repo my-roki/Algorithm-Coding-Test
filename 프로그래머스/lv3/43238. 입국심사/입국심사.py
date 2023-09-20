839aafa86b9c7b0a3dbdedc7f6653101272316bc
cryoon <yoonchangrock@gmail.com>
2023-09-20 11:47:36 +0900

    [level 3] Title: 입국심사, Time: 0.05 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv3/43238. 입국심사/입국심사.py
+++ 프로그래머스/lv3/43238. 입국심사/입국심사.py
@@ -1,21 +1,40 @@
-import sys
+from typing import List
 
-def solution(n, times):
-    answer = sys.maxsize
-    left, right = 1, max(times) * n
-    while left <= right:
+
+def solution(n: int, times: List[int]) -> int:
+    # 이진 탐색
+
+    # 1. left = 1, right = 심사시간 최댓값 * n
+    # 1-1. 심사시간 최댓값은 가장 오래 걸리는 심사관이 모든 사람을 심사할 때이다.
+    # 1-2. 심사시간의 모든 경우의 수를 가지고 가장 적게 걸리는 시간을 탐색.
+
+    # 2. 중간값을 기준으로 특정 시간동안 심사할 수 있는 사람의 수를 구한다.
+    # 2-1. 중간값에서 각 심사위원이 심사할 수 있는 사람의 수를 구한다.
+    # 2-2. 중간값에서 각 심사위원이 심사할 수 있는 사람의 수 의 합(sum_people)에 따라 left, right를 조정한다.
+    # 2-3. sum_people < n 이면 left = mid + 1, sum_people >= n 이면 right = mid
+
+    # 3. left == right가 될 때까지 반복한다. => left가 최소 시간이 된다.
+
+    left = 1
+    right = max(times) * n
+
+    while left != right:
         mid = (left + right) // 2
-        done = 0
+        sum_people = 0
         for time in times:
-            done += mid // time
-
-            if done >= n:
+            sum_people += mid // time
+            if sum_people >= n:
                 break
 
-        if done >= n:
-            answer = min(answer, mid)
-            right = mid - 1
+        if sum_people >= n:
+            right = mid
         else:
             left = mid + 1
 
-    return answer
+    return left
+
+
+if __name__ == "__main__":
+    n = 6
+    times = [7, 10]
+    print(solution(n, times))  # 28
