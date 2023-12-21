1533b6c40cb1b7233341f67aef7fc2b56e3da3b9
cryoon <yoonchangrock@gmail.com>
2023-12-21 10:18:26 +0900

    [Silver V] Title: 영화감독 숌, Time: 828 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -1,11 +1,11 @@
 import sys
 
-for N in sys.stdin.readlines():
-    n = 1
-    result = 666
-    while n < int(N):
-        result += 1
-        if "666" in str(result):
-            n += 1
-
-    print(result)
+if __name__ == "__main__":
+    N = int(sys.stdin.readline().rstrip())
+    init = 666
+    while N != 1:
+        init += 1
+        if "666" in str(init):
+            N -= 1
+    
+    print(init)
